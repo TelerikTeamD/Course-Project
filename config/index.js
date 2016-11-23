205c3c5438d88f0442b91a0549e9db49532526da
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-11-23 12:15:04 +0200

    Update

--- config/index.js
+++ config/index.js
@@ -1,6 +1,11 @@
 /* globals module */
 "use strict";
 
+// const protocol = "mongodb:/";
+// const server = "localhost:27017";
+// const databaseName = "NewDB";
+// const connectionString = `${protocol}/${server}/${databaseName}`;
+
 module.exports = {
     port: 3333,
     connectionString: "mongodb://localhost/superheroesDb"
