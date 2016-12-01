d46259e8ce5e95ba87d2250ca509624bf7a42738
pepchon <petarstanchev@hotmail.com>
2016-12-01 14:16:36 +0200

    Making views and testing user stuff  (/account) . Everything look like mess but after all tested gonna refactor .

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const config = require("./config/data");
 
 const app = require("./config/application");
 const data = require("./data")(config);
+const passportConfig = require('./config/passport/index');
 
 const userController = require('./controllers/user-controller');
 const apiController = require('./controllers/api-controller');
@@ -13,8 +14,19 @@ const apiController = require('./controllers/api-controller');
 require("./routers")(app, data);
 
 app.get('/login', userController.getLogin);
+app.post('/login', userController.postLogin);
+app.get('/logout', userController.logout);
+app.get('/forgot', userController.getForgot);
+app.post('/forgot', userController.postForgot);
+app.get('/reset/:token', userController.getReset);
+app.post('/reset/:token', userController.postReset);
 app.get('/signup', userController.getSignup);
 app.post('/signup', userController.postSignup);
+app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
+app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
+app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
+app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
+
 
 app.listen(config.port, () => {
     console.log(`Running app at port ${config.port}`)
--- config/application/index.js
+++ config/application/index.js
@@ -1,10 +1,89 @@
 /* globals module require */
 "use strict";
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const compression = require('compression');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const chalk = require('chalk');
+const errorHandler = require('errorhandler');
+const lusca = require('lusca');
+const dotenv = require('dotenv');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('express-flash');
+const path = require('path');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const expressValidator = require('express-validator');
+const expressStatusMonitor = require('express-status-monitor');
+const sass = require('node-sass-middleware');
+const multer = require('multer');
+
+const upload = multer({ dest: path.join(__dirname, 'uploads') });
+
+dotenv.load({ path: 'test.env.example' });
+
+const userController = require('../../controllers/user-controller');
+const apiController = require('../../controllers/api-controller');
+
+
 
 let app = express();
+
+app.set('port', process.env.PORT || 3333);
+
+app.set('view engine', 'pug');
+app.use(expressStatusMonitor());
+app.use(compression());
+app.use(sass({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public')
+}));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    store: new MongoStore({
+        url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
+        autoReconnect: true
+    })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use((req, res, next) => {
+    if (req.path === '/api/upload') {
+        next();
+    } else {
+        lusca.csrf()(req, res, next);
+    }
+});
+app.use(lusca.xframe('SAMEORIGIN'));
+app.use(lusca.xssProtection(true));
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+app.use((req, res, next) => {
+    // After successful login, redirect back to the intended page
+    if (!req.user &&
+        req.path !== '/login' &&
+        req.path !== '/signup' &&
+        !req.path.match(/^\/auth/) &&
+        !req.path.match(/\./)) {
+        req.session.returnTo = req.path;
+    } else if (req.user &&
+        req.path == '/account') {
+        req.session.returnTo = req.path;
+    }
+    next();
+});
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 app.set("view engine", "pug");
 
