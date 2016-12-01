609c2873b1ab0563a5d7400575cfd64962809ccb
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-12-01 22:31:02 +0200

    adding team model

---
+++ models/team-model.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+let schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    yearFounded: {
+        type: Number,
+        required: true
+    },
+    manager: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    nickName: {
+        type: String,
+        required: true
+    },
+    stadiumName: {
+        type: String,
+        required: true
+    }
+});
+mongoose.model("Team", schema, "teams");
+
+module.exports = mongoose.model("Team");
